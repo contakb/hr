58e0dae4f2fd8356747c7ff065f690bf80d49bd5
contakb~ <contakb@gmail.com>
2023-09-14 15:48:16 +0200

    dane firmy ustawienia

--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -10,36 +10,84 @@ function SettingsPage() {
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     const newValue = type === 'checkbox' ? checked : value;
-
+  
     setFormData({
       ...formData,
       [name]: newValue,
     });
   };
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission, e.g., send data to the server
+    // formData now contains all the entered details
+    console.log(formData); // Replace with your API call
   };
-
   return (
     <div>
-      <h1>Settings</h1>
+      <h1>Company Settings</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          Username:
+          Company Name:
           <input
             type="text"
-            name="username"
-            value={formData.username}
+            name="companyName"
+            value={formData.companyName}
             onChange={handleChange}
           />
         </label>
-        {/* Add more form fields */}
+        <label>
+          Forma Prawna:
+          <select
+            name="formaPrawna"
+            value={formData.formaPrawna}
+            onChange={handleChange}
+          >
+            <option value="osobaPrawna">Osoba Prawna</option>
+            <option value="osobaFizyczna">Osoba Fizyczna</option>
+          </select>
+        </label>
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Tax ID Number:
+          <input
+            type="text"
+            name="taxId"
+            value={formData.taxId}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Tax Office:
+          <input
+            type="text"
+            name="taxOffice"
+            value={formData.taxOffice}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Bank Account:
+          <input
+            type="text"
+            name="bankAccount"
+            value={formData.bankAccount}
+            onChange={handleChange}
+          />
+        </label>
+        {/* Add more fields for other details */}
         <button type="submit">Save</button>
       </form>
     </div>
   );
-}
+}  
 
 export default SettingsPage;
