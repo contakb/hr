22cef6123e3ee290c06b0f7551674dfdd1fa17fe
contakb~ <contakb@gmail.com>
2023-11-03 14:41:41 +0100

    zmiana emplyparam

--- src/EmployeeParam.js
+++ src/EmployeeParam.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
@@ -7,12 +7,17 @@ function EmployeeParam() {
   const navigate = useNavigate();
 
   // New state variables for the employee parameters
-  const [koszty, setKoszty] = useState('');
-  const [ulga, setUlga] = useState('');
+  const [koszty, setKoszty] = useState('250');
+  const [ulga, setUlga] = useState('300');
   const [kodUb, setKodUb] = useState('');
   const [validFrom, setValidFrom] = useState('');
   // Add a state to store the response data for the notification
   const [paramData, setParamData] = useState(null);
+  const [worksOutsideHome, setWorksOutsideHome] = useState(false);
+const [hasPension, setHasPension] = useState(false);
+const [numOfCompanies, setNumOfCompanies] = useState(1);
+
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -53,13 +58,100 @@ function EmployeeParam() {
   const handleKodUbChange = (event) => setKodUb(event.target.value);
   const handleValidFromChange = (event) => setValidFrom(event.target.value);
 
+  const handleWorksOutsideHomeChange = (event) => {
+    const answer = event.target.value === 'yes';
+    setWorksOutsideHome(answer);
+    setKoszty(answer ? '300' : '250'); // Automatically set koszty based on the answer
+  };
+  
+  const handleHasPensionChange = (event) => {
+    const answer = event.target.value === 'yes';
+    setHasPension(answer);
+    setUlga(calculateUlga(answer, numOfCompanies));
+  };
+  
+  const handleNumOfCompaniesChange = (event) => {
+    const count = Number(event.target.value);
+    setNumOfCompanies(count);
+    setUlga(calculateUlga(hasPension, count));
+  };
+  
+
+  const calculateUlga = (hasPension, numOfCompanies) => {
+    if (hasPension) {
+      return '0'; // If the employee has a pension, ulga is 0
+    } else {
+      // No pension, determine ulga based on the number of companies
+      switch (numOfCompanies) {
+        case 1:
+          return '300';
+        case 2:
+          return '150';
+        case 3:
+          return '100';
+        default:
+          return '300'; // Default to 300 if something goes wrong
+      }
+    }
+  };
+  // Initialize ulga with default value based on conditions
+useEffect(() => {
+    setUlga(calculateUlga(hasPension, numOfCompanies));
+  }, [hasPension, numOfCompanies]); // Recalculate when hasPension or numOfCompanies changes
+  
+  
+
   // Below is the form where you can input the parameters
   return (
     <div>
       <h2>Add Employee Parameters</h2>
       <form onSubmit={handleSubmit}>
+      <label>Czy pracownik pracuje poza miejscem zamieszkania?</label>
+<div>
+  <input
+    type="radio"
+    name="worksOutsideHome"
+    value="yes"
+    checked={worksOutsideHome === true}
+    onChange={handleWorksOutsideHomeChange}
+  /> Tak
+  <input
+    type="radio"
+    name="worksOutsideHome"
+    value="no"
+    checked={worksOutsideHome === false}
+    onChange={handleWorksOutsideHomeChange}
+  /> Nie
+</div>
         <label>Koszty uzyskania przychodu:</label>
         <input type="number" value={koszty} onChange={handleKosztyChange} />
+
+        <label>Posiada rentę/emerytrę?</label>
+<div>
+  <input
+    type="radio"
+    name="hasPension"
+    value="yes"
+    checked={hasPension === true}
+    onChange={handleHasPensionChange}
+  /> Tak
+  <input
+    type="radio"
+    name="hasPension"
+    value="no"
+    checked={hasPension === false}
+    onChange={handleHasPensionChange}
+  /> Nie
+</div>
+{/* Question for the number of companies */}
+<label>Ilość firm, w których pracuje:</label>
+<select value={numOfCompanies} onChange={handleNumOfCompaniesChange}>
+  <option value={1}>1 firma</option>
+  <option value={2}>2 firmy</option>
+  <option value={3}>3 firmy</option>
+  {/* Add more options if needed */}
+</select>
+
 
         <label>Ulga podatkowa:</label>
         <input type="number" value={ulga} onChange={handleUlgaChange} />
